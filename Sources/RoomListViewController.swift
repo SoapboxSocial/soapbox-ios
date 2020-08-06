175de76401688dcdf043e2e9d643db49296471dc
decanus <7621705+decanus@users.noreply.github.com>
2020-08-06 22:35:41 +0200

    added todo

--- Sources/RoomListViewController.swift
+++ Sources/RoomListViewController.swift
@@ -60,7 +60,7 @@ class RoomListViewController: UIViewController {
         let item = UIBarButtonItem(title: "@" + UserDefaults.standard.string(forKey: "username")!, style: .plain, target: nil, action: nil)
         item.tintColor = .black
         navigationItem.leftBarButtonItem = item
-        navigationController?.hidesBarsOnSwipe = true
+        navigationController?.hidesBarsOnSwipe = true // @todo this seems a little unstable
     }
 
     @objc private func didPullToRefresh() {
