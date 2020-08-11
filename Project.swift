7db63b670b289ac819f2e318dabe7bd31c71a194
decanus <7621705+decanus@users.noreply.github.com>
2020-08-11 09:58:59 +0200

    upsert

--- Project.swift
+++ Project.swift
@@ -18,7 +18,7 @@ let project = Project(
             settings: Settings(
                 base: [
                     "ENABLE_BITCODE": SettingValue.string("NO"),
-                    "CURRENT_PROJECT_VERSION": SettingValue.string("28"),
+                    "CURRENT_PROJECT_VERSION": SettingValue.string("29"),
                     "MARKETING_VERSION": SettingValue.string("1.3"),
                 ]
             )
