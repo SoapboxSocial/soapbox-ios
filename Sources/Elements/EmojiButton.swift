6ea39f02ee3152ea2f406ed81df61cb1f1a7bd86
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2020-09-10 18:26:04 +0200

    feature/room-utility-buttons-sf-icons (#115)

--- Sources/Elements/EmojiButton.swift
+++ Sources/Elements/EmojiButton.swift
@@ -1,19 +1,17 @@
 import UIKit
 
 class EmojiButton: UIButton {
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
     }
-    
-    required init?(coder: NSCoder) {
+
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setup() {
         backgroundColor = .background
         layer.cornerRadius = frame.size.width / 2
     }
 }
-
