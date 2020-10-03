443f06a3edd0df60bb4e897b7f5b6dc1a9a008e3
decanus <7621705+decanus@users.noreply.github.com>
2020-10-03 16:28:47 +0200

    themeing

--- Sources/Extensions/UIColor.swift
+++ Sources/Extensions/UIColor.swift
@@ -29,18 +29,6 @@ extension UIColor {
         UIColor(red: 133 / 255, green: 90 / 255, blue: 255 / 255, alpha: 1)
     }
 
-//    static var background: UIColor {
-//        UIColor(red: 236 / 255, green: 239 / 255, blue: 253 / 255, alpha: 1)
-//
-//            (UITraitCollection: UITraitCollection) -> UIColor in
-//            if UITraitCollection.userInterfaceStyle == .dark {
-//                return UIColor(red: 14 / 255, green: 14 / 255, blue: 15 / 255, alpha: 1)
-//            } else {
-//                return UIColor(red: 250 / 255, green: 250 / 255, blue: 250 / 255, alpha: 1)
-//            }
-//        }
-//    }
-
     // @todo there are probably better names for the following 2
     static var buttonBackground: UIColor {
         UIColor(red: 170 / 255, green: 139 / 255, blue: 255 / 255, alpha: 1)
