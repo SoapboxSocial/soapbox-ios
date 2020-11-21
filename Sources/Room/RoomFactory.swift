34e1ea9584003bf47b33e2c72cda2a681c58a682
decanus <7621705+decanus@users.noreply.github.com>
2020-11-21 14:36:48 +0100

    made things more reliable

--- Sources/Room/RoomFactory.swift
+++ Sources/Room/RoomFactory.swift
@@ -1,22 +1,34 @@
 import Foundation
 import GRPC
 import SwiftProtobuf
+import WebRTC
 
 class RoomFactory {
-    static func createRoom() -> Room {
-        return Room(
-            rtc: newRTCClient(),
-            grpc: ServiceFactory.createRoomService()
-        )
+    private static let defaults = RTCIceServer(urlStrings: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302",
+    ])
+
+    static func create(callback: @escaping (Room) -> Void) {
+        webrtc(callback: { client in
+            callback(Room(
+                rtc: client,
+                grpc: ServiceFactory.createRoomService()
+            ))
+        })
     }
 
-    private static func newRTCClient() -> WebRTCClient {
-        return WebRTCClient(iceServers: [
-            "stun:stun.l.google.com:19302",
-            "stun:stun1.l.google.com:19302",
-            "stun:stun2.l.google.com:19302",
-            "stun:stun3.l.google.com:19302",
-            "stun:stun4.l.google.com:19302",
-        ])
+    private static func webrtc(callback: @escaping (WebRTCClient) -> Void) {
+        Twilio.nts(callback: { result in
+            switch result {
+            case .failure:
+                callback(WebRTCClient(iceServers: [RoomFactory.defaults]))
+            case let .success(servers):
+                callback(WebRTCClient(iceServers: servers))
+            }
+        })
     }
 }
