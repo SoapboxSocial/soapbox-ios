cf457046d72e2c770be017aad5d9106716f52009
decanus <7621705+decanus@users.noreply.github.com>
2020-12-09 16:16:52 +0100

    started working on player

---
+++ Sources/Stories/StoryPlayer.swift
@@ -0,0 +1,49 @@
+import AVFoundation
+
+protocol StoryPlayerDelegate {
+    func startedPlaying(story: APIClient.Story)
+    func didReachEnd()
+}
+
+class StoryPlayer {
+    private let player = AVQueuePlayer()
+
+    private var currentItem = -1
+
+    private let items: [APIClient.Story]
+
+    var delegate: StoryPlayerDelegate?
+
+    init(items: [APIClient.Story]) {
+        self.items = items
+
+        for item in items {
+            let url = Configuration.cdn.appendingPathComponent("/stories/" + item.id + ".aac")
+            player.insert(AVPlayerItem(asset: AVURLAsset(url: url)), after: nil)
+        }
+    }
+
+    func play() {
+        NotificationCenter.default.addObserver(self, selector: #selector(itemDidPlayToEnd), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        player.play()
+    }
+
+    func stop() {
+        player.pause()
+    }
+
+    func duration() -> Float {
+        return player.duration()
+    }
+
+    @objc private func itemDidPlayToEnd() {
+        currentItem += 1
+
+        if currentItem == items.count - 1 {
+            delegate?.didReachEnd()
+            return
+        }
+
+        delegate?.startedPlaying(story: items[currentItem])
+    }
+}
