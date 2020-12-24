ebc991c0bdc06750b205b170d6177d658b879823
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2020-12-24 23:03:59 +0100

    fix/room-button-shadow (#340)

--- Sources/Elements/CreateRoomButton.swift
+++ Sources/Elements/CreateRoomButton.swift
@@ -10,11 +10,13 @@ class CreateRoomButton: ButtonWithSpringAnimation {
         layer.cornerRadius = frame.size.height / 2
 
         layer.masksToBounds = false
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius).cgPath
-        layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-        layer.shadowOpacity = 0.5
-        layer.shadowRadius = 1.0
+
+        layer.addSublayer(shadowLayer(alpha: 0.02, radius: 1.94, height: 2.42))
+        layer.addSublayer(shadowLayer(alpha: 0.03, radius: 4.66, height: 5.82))
+        layer.addSublayer(shadowLayer(alpha: 0.04, radius: 8.77, height: 10.96))
+        layer.addSublayer(shadowLayer(alpha: 0.04, radius: 15.64, height: 19.54))
+        layer.addSublayer(shadowLayer(alpha: 0.05, radius: 29.24, height: 36.56))
+        layer.addSublayer(shadowLayer(alpha: 0.07, radius: 70, height: 87.5))
 
         setImage(
             UIImage(systemName: "quote.bubble.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30, weight: .bold)),
@@ -27,5 +29,20 @@ class CreateRoomButton: ButtonWithSpringAnimation {
 
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func shadowLayer(alpha: CGFloat, radius: CGFloat, height: CGFloat) -> CALayer {
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: 0)
+
+        let layer = CALayer()
+        layer.shadowPath = shadowPath.cgPath
+        layer.shadowColor = UIColor.black.withAlphaComponent(alpha).cgColor
+        layer.shadowOpacity = 1
+        layer.shadowRadius = radius
+        layer.shadowOffset = CGSize(width: 0, height: height)
+        layer.bounds = bounds
+        layer.position = center
+
+        return layer
     }
 }
