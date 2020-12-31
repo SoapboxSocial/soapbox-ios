fcb1a2d0d881726c9354d3567bc14c54a5457d52
decanus <7621705+decanus@users.noreply.github.com>
2020-12-31 09:18:24 +0100

    rm

--- Sources/Scenes/Authentication/AuthenticationViewController.swift
+++ Sources/Scenes/Authentication/AuthenticationViewController.swift
@@ -14,10 +14,6 @@ protocol AuthenticationViewControllerOutput {
 class AuthenticationViewController: UIPageViewController {
     var output: AuthenticationViewControllerOutput!
 
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-
     private var orderedViewControllers = [UIViewController]()
 
     private var state = AuthenticationInteractor.AuthenticationState.getStarted
