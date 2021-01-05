2193cb58fa4ca04ef29c473c70f637be958a2da5
decanus <7621705+decanus@users.noreply.github.com>
2021-01-05 18:21:55 +0100

    fix

--- Sources/Room/Service/RoomClient.swift
+++ Sources/Room/Service/RoomClient.swift
@@ -3,3 +3,7 @@ import Foundation
 final class RoomClient {
     // @TODO THIS CONTAINS WEBRTC AND SIGNALING LOGIC.
 }
+
+extension RoomClient: SignalingClientDelegate {
+    func signalClient(_: SignalingClient, didReceiveTrickle _: Trickle) {}
+}
--- Sources/Room/Service/SignalingClient.swift
+++ Sources/Room/Service/SignalingClient.swift
@@ -2,28 +2,27 @@ import Foundation
 import GRPC
 import WebRTC
 
-protocol SignalingClientDelegate: class {
-    func signalClient(_ signalClient: SignalingClient, didReceiveTrickle: Trickle)
+protocol SignalingClientDelegate: AnyObject {
+    func signalClient(_ signalClient: SignalingClient, didReceiveTrickle trickle: Trickle)
 }
 
 final class SignalingClient {
-    
     private let grpc: SFUClient
     private var stream: BidirectionalStreamingCall<SignalRequest, SignalReply>!
-    
+
     weak var delegate: SignalingClientDelegate?
-    
+
     init(grpc: SFUClient) {
         self.grpc = grpc
         stream = grpc.signal(handler: handle)
     }
-    
+
     func create() {}
 
     func join(id _: String) {}
 
     func trickle() {}
-    
+
     private func handle(_ reply: SignalReply) {
         switch reply.payload {
         case .join:
