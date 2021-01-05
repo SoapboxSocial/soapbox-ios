84d4f5756c080bc0c172e2013e6543b995ad0a6e
decanus <7621705+decanus@users.noreply.github.com>
2021-01-05 18:18:44 +0100

    some shifting

---
+++ Sources/Room/Service/RoomClient.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+final class RoomClient {
+    // @TODO THIS CONTAINS WEBRTC AND SIGNALING LOGIC.
+}
---
+++ Sources/Room/Service/SignalingClient.swift
@@ -0,0 +1,45 @@
+import Foundation
+import GRPC
+import WebRTC
+
+protocol SignalingClientDelegate: class {
+    func signalClient(_ signalClient: SignalingClient, didReceiveTrickle: Trickle)
+}
+
+final class SignalingClient {
+    
+    private let grpc: SFUClient
+    private var stream: BidirectionalStreamingCall<SignalRequest, SignalReply>!
+    
+    weak var delegate: SignalingClientDelegate?
+    
+    init(grpc: SFUClient) {
+        self.grpc = grpc
+        stream = grpc.signal(handler: handle)
+    }
+    
+    func create() {}
+
+    func join(id _: String) {}
+
+    func trickle() {}
+    
+    private func handle(_ reply: SignalReply) {
+        switch reply.payload {
+        case .join:
+            break
+        case .create:
+            break
+        case .description_p:
+            break
+        case let .trickle(trickle):
+            delegate?.signalClient(self, didReceiveTrickle: trickle)
+        case .iceConnectionState:
+            break
+        case .error:
+            break
+        case .none:
+            break
+        }
+    }
+}
