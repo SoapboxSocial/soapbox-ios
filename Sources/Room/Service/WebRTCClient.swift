d57503cb527f2123a4169a30a9fc231749870fb4
decanus <7621705+decanus@users.noreply.github.com>
2021-01-07 18:21:52 +0100

    cleanup

--- Sources/Room/Service/WebRTCClient.swift
+++ Sources/Room/Service/WebRTCClient.swift
@@ -61,8 +61,7 @@ final class WebRTCClient: NSObject {
     }
 
     func offer(completion: @escaping (_ sdp: RTCSessionDescription) -> Void) {
-        let constrains = RTCMediaConstraints(mandatoryConstraints: mediaConstrains,
-                                             optionalConstraints: nil)
+        let constrains = RTCMediaConstraints(mandatoryConstraints: mediaConstrains, optionalConstraints: nil)
         peerConnection.offer(for: constrains) { sdp, _ in
             guard let sdp = sdp else {
                 return
