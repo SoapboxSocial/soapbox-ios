c9cef37d11af06cbbc5bb25b900ad3463e0b6191
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2021-01-20 10:56:53 +0100

    enhancement/user-by-username (#372)

--- Sources/Scenes/Profile/ProfileInteractor.swift
+++ Sources/Scenes/Profile/ProfileInteractor.swift
@@ -16,15 +16,17 @@ protocol ProfileInteractorOutput {
 class ProfileInteractor {
     private let output: ProfileInteractorOutput
     private let api: APIClient
-    private let user: Int
+    private var user: Int?
+    private var username: String?
 
     private var groupOffset = 0
     private let groupLimit = 10
 
-    init(output: ProfileInteractorOutput, api: APIClient, user: Int) {
+    init(output: ProfileInteractorOutput, api: APIClient, user: Int? = nil, username: String? = nil) {
         self.output = output
         self.api = api
         self.user = user
+        self.username = username
     }
 }
 
@@ -32,7 +34,11 @@ extension ProfileInteractor: ProfileViewControllerOutput {
     func loadMoreGroups() {
         let nextOffset = groupOffset + groupLimit
 
-        api.groups(id: user, limit: groupLimit, offset: nextOffset, callback: { result in
+        guard let id = user else {
+            return
+        }
+
+        api.groups(id: id, limit: groupLimit, offset: nextOffset, callback: { result in
             switch result {
             case .failure: break
             case let .success(groups):
@@ -43,7 +49,21 @@ extension ProfileInteractor: ProfileViewControllerOutput {
     }
 
     func loadData() {
-        api.user(id: user) { result in
+        if user != nil {
+            return loadById()
+        }
+
+        if username != nil {
+            return loadByUsername()
+        }
+    }
+
+    private func loadById() {
+        guard let id = user else {
+            return
+        }
+
+        api.user(id: id) { result in
             switch result {
             case .failure:
                 self.output.displayError()
@@ -57,15 +77,55 @@ extension ProfileInteractor: ProfileViewControllerOutput {
             }
         }
 
-        api.groups(id: user, limit: groupLimit, offset: groupOffset, callback: { result in
+        loadGroups()
+        loadStories()
+    }
+
+    private func loadByUsername() {
+        guard let name = username else {
+            return
+        }
+
+        api.user(name: name) { result in
+            switch result {
+            case .failure:
+                self.output.displayError()
+            case let .success(user):
+                self.user = user.id
+
+                self.loadGroups()
+                self.loadStories()
+
+                if self.user == UserDefaults.standard.integer(forKey: UserDefaultsKeys.userId) {
+                    self.output.displayPersonal(profile: user)
+                    return
+                }
+
+                self.output.display(profile: user)
+            }
+        }
+    }
+
+    private func loadGroups() {
+        guard let id = user else {
+            return
+        }
+
+        api.groups(id: id, limit: groupLimit, offset: groupOffset, callback: { result in
             switch result {
             case .failure: break
             case let .success(groups):
                 self.output.display(groups: groups)
             }
         })
+    }
 
-        api.stories(user: user, callback: { result in
+    private func loadStories() {
+        guard let id = user else {
+            return
+        }
+
+        api.stories(user: id, callback: { result in
             switch result {
             case .failure: break
             case let .success(stories):
@@ -75,7 +135,11 @@ extension ProfileInteractor: ProfileViewControllerOutput {
     }
 
     func follow() {
-        api.follow(id: user, callback: { result in
+        guard let id = user else {
+            return
+        }
+
+        api.follow(id: id, callback: { result in
             switch result {
             case .failure:
                 self.output.displayError()
@@ -86,7 +150,11 @@ extension ProfileInteractor: ProfileViewControllerOutput {
     }
 
     func unfollow() {
-        api.unfollow(id: user, callback: { result in
+        guard let id = user else {
+            return
+        }
+
+        api.unfollow(id: id, callback: { result in
             switch result {
             case .failure:
                 self.output.displayError()
@@ -97,7 +165,11 @@ extension ProfileInteractor: ProfileViewControllerOutput {
     }
 
     func block() {
-        api.block(user: user, callback: { result in
+        guard let id = user else {
+            return
+        }
+
+        api.block(user: id, callback: { result in
             switch result {
             case .failure:
                 self.output.displayError()
@@ -108,7 +180,11 @@ extension ProfileInteractor: ProfileViewControllerOutput {
     }
 
     func unblock() {
-        api.unblock(user: user, callback: { result in
+        guard let id = user else {
+            return
+        }
+
+        api.unblock(user: id, callback: { result in
             switch result {
             case .failure:
                 self.output.displayError()
