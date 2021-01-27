0c93a437ef0011583dd5aa1b54b59b8d6b5ccdac
decanus <7621705+decanus@users.noreply.github.com>
2021-01-27 16:48:50 +0100

    deduplicate

--- Sources/API/APIClient.swift
+++ Sources/API/APIClient.swift
@@ -1,54 +1,8 @@
 import Alamofire
 import Foundation
 import KeychainAccess
-import WebRTC
 
-class APIClient {
-    // @todo put elsewhere?
-    private var token: String? {
-        guard let identifier = Bundle.main.bundleIdentifier else {
-            fatalError("no identifier")
-        }
-
-        let keychain = Keychain(service: identifier)
-        return keychain[string: "token"]
-    }
-
-    enum ErrorCode: Int, Decodable {
-        case invalidRequestBody
-        case missingParameter
-        case failedToRegister
-        case invalidEmail
-        case invalidUsername
-        case usernameAlreadyExists
-        case failedToLogin
-        case incorrectPin
-        case userNotFound
-        case failedToGetUser
-        case failedToGetFollowers
-        case unauthorized
-        case failedToStoreDevice
-        case notFound
-        case notAllowed
-    }
-
-    struct ErrorResponse: Decodable {
-        let code: ErrorCode
-        let message: String
-    }
-
-    enum Error: Swift.Error {
-        case preprocessing
-        case decode
-        case requestFailed
-        case endpoint(ErrorResponse)
-        case other(AFError)
-    }
-
-    let decoder = JSONDecoder()
-}
-
-extension APIClient {
+class APIClient: Client {
     enum LoginState: String, Decodable {
         case register
         case success
@@ -88,6 +42,10 @@ extension APIClient {
         private enum CodingKeys: String, CodingKey {
             case state, expiresIn = "expires_in", user, token
         }
+    }
+
+    init() {
+        super.init(url: Configuration.rootURL)
     }
 
     func login(email: String, callback: @escaping (Result<String, Error>) -> Void) {
@@ -546,71 +504,5 @@ extension APIClient {
 
     func unblock(user: Int, callback: @escaping (Result<Void, Error>) -> Void) {
         void(path: "/v1/blocks/", method: .delete, parameters: ["id": user], callback: callback)
-    }
-}
-
-extension APIClient {
-    private func get<T: Decodable>(path: String, parameters: Parameters? = nil, callback: @escaping (Result<T, Error>) -> Void) {
-        AF.request(
-            Configuration.rootURL.appendingPathComponent(path),
-            method: .get,
-            parameters: parameters,
-            encoding: URLEncoding.default,
-            headers: ["Authorization": self.token!]
-        )
-        .validate()
-        .response { result in
-            self.decodable(result, callback: callback)
-        }
-    }
-
-    private func post(path: String, parameters: Parameters? = nil, callback: @escaping (Result<Void, Error>) -> Void) {
-        void(path: path, method: .post, parameters: parameters, callback: callback)
-    }
-
-    private func void(path: String, method: HTTPMethod, parameters: Parameters? = nil, callback: @escaping (Result<Void, Error>) -> Void) {
-        AF.request(
-            Configuration.rootURL.appendingPathComponent(path),
-            method: method,
-            parameters: parameters,
-            encoding: URLEncoding.default,
-            headers: ["Authorization": self.token!]
-        )
-        .validate()
-        .response { result in
-            if let err = self.validate(result) {
-                return callback(.failure(err))
-            }
-
-            return callback(.success(()))
-        }
-    }
-
-    private func decodable<T: Decodable>(_ response: AFDataResponse<Data?>, callback: @escaping (Result<T, Error>) -> Void) {
-        if let err = validate(response) {
-            return callback(.failure(err))
-        }
-
-        do {
-            return callback(.success(try decoder.decode(T.self, from: response.data!)))
-        } catch {
-            return callback(.failure(.decode))
-        }
-    }
-
-    private func validate(_ response: AFDataResponse<Data?>) -> Error? {
-        guard case let .failure(err) = response.result else {
-            return nil
-        }
-
-        guard let data = response.data else {
-            return .other(err)
-        }
-
-        do {
-            return .endpoint(try decoder.decode(ErrorResponse.self, from: data))
-        } catch {
-            return (.other(err))
-        }
     }
 }
--- Sources/API/RoomAPIClient.swift
+++ Sources/API/RoomAPIClient.swift
@@ -2,52 +2,7 @@ import Alamofire
 import Foundation
 import KeychainAccess
 
-// @TODO deduplicate with APICLient
-
-class RoomAPIClient {
-    enum ErrorCode: Int, Decodable {
-        case invalidRequestBody
-        case missingParameter
-        case failedToRegister
-        case invalidEmail
-        case invalidUsername
-        case usernameAlreadyExists
-        case failedToLogin
-        case incorrectPin
-        case userNotFound
-        case failedToGetUser
-        case failedToGetFollowers
-        case unauthorized
-        case failedToStoreDevice
-        case notFound
-        case notAllowed
-    }
-
-    struct ErrorResponse: Decodable {
-        let code: ErrorCode
-        let message: String
-    }
-
-    enum Error: Swift.Error {
-        case preprocessing
-        case decode
-        case requestFailed
-        case endpoint(ErrorResponse)
-        case other(AFError)
-    }
-
-    let decoder = JSONDecoder()
-
-    // @todo put elsewhere?
-    private var token: String? {
-        guard let identifier = Bundle.main.bundleIdentifier else {
-            fatalError("no identifier")
-        }
-
-        let keychain = Keychain(service: identifier)
-        return keychain[string: "token"]
-    }
-
+class RoomAPIClient: Client {
     struct Member: Decodable {
         let id: Int
         let displayName: String
@@ -64,49 +19,11 @@ class RoomAPIClient {
         let members: [Member]
     }
 
+    init() {
+        super.init(url: Configuration.roomAPIURL)
+    }
+
     func rooms(callback: @escaping (Result<[Room], Error>) -> Void) {
         get(path: "/v1/rooms", callback: callback)
-    }
-
-    private func get<T: Decodable>(path: String, parameters: Parameters? = nil, callback: @escaping (Result<T, Error>) -> Void) {
-        AF.request(
-            Configuration.roomAPIURL.appendingPathComponent(path),
-            method: .get,
-            parameters: parameters,
-            encoding: URLEncoding.default,
-            headers: ["Authorization": self.token!]
-        )
-        .validate()
-        .response { result in
-            self.decodable(result, callback: callback)
-        }
-    }
-
-    private func decodable<T: Decodable>(_ response: AFDataResponse<Data?>, callback: @escaping (Result<T, Error>) -> Void) {
-        if let err = validate(response) {
-            return callback(.failure(err))
-        }
-
-        do {
-            return callback(.success(try decoder.decode(T.self, from: response.data!)))
-        } catch {
-            return callback(.failure(.decode))
-        }
-    }
-
-    private func validate(_ response: AFDataResponse<Data?>) -> Error? {
-        guard case let .failure(err) = response.result else {
-            return nil
-        }
-
-        guard let data = response.data else {
-            return .other(err)
-        }
-
-        do {
-            return .endpoint(try decoder.decode(ErrorResponse.self, from: data))
-        } catch {
-            return (.other(err))
-        }
     }
 }
