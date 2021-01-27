7bf4440ab32c75127261db81e1447e7c1a22aaa6
decanus <7621705+decanus@users.noreply.github.com>
2021-01-27 23:32:43 +0100

    rm

--- Sources/Configuration.swift
+++ Sources/Configuration.swift
@@ -21,26 +21,6 @@ class Configuration {
         url(key: "ROOM_API_URL")
     }()
 
-    static let roomServiceURL: String = {
-        guard let urlString = Configuration.infoDictionary["ROOM_SERVICE_URL"] as? String else {
-            fatalError("ROOM_SERVICE_URL not set in plist for this environment")
-        }
-
-        return urlString
-    }()
-
-    static let roomServicePort: Int = {
-        guard let value = Configuration.infoDictionary["ROOM_SERVICE_PORT"] as? String else {
-            fatalError("ROOM_SERVICE_PORT not set in plist for this environment")
-        }
-
-        guard let port = Int(value) else {
-            fatalError("\(value) could not be converted")
-        }
-
-        return port
-    }()
-
     private static func url(key: String) -> URL {
         guard let urlString = Configuration.infoDictionary[key] as? String else {
             fatalError("\(key) not set in plist for this environment")
