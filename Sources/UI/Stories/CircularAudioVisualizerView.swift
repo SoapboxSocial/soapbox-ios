5c81c07b69de5503a82c8c986dc3fcce207fc84a
decanus <7621705+decanus@users.noreply.github.com>
2021-03-05 18:31:11 +0100

    unused

--- Sources/UI/Stories/CircularAudioVisualizerView.swift
+++ Sources/UI/Stories/CircularAudioVisualizerView.swift
@@ -2,14 +2,10 @@ import AVFoundation
 import UIKit
 
 class CircularAudioVisualizerView: UIView {
-    // Configuration Settings
-    private let updateInterval = 0.05
+   
     private let animationDuration = 0.02
     private let maxPowerDelta: CGFloat = 30
     private let minScale: CGFloat = 0.6
-
-    // Ingected Player to get power Metrics
-    weak var player: AVAudioPlayer!
 
     func update(power: Double) {
         UIView.animate(withDuration: animationDuration, animations: {
@@ -19,8 +15,9 @@ class CircularAudioVisualizerView: UIView {
 
     private func animate(to power: Double) {
         let powerDelta = (maxPowerDelta + CGFloat(power)) * 2 / 100
-        let compute: CGFloat = minScale + powerDelta
-        let scale: CGFloat = CGFloat.maximum(compute, minScale)
+        let compute = minScale + powerDelta
+        let scale = CGFloat.maximum(compute, minScale)
+        
         transform = CGAffineTransform(scaleX: scale, y: scale)
     }
 
