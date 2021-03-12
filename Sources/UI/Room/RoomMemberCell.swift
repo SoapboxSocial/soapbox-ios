6274710d6fe9c0d0d2a610f008423e23f365d469
decanus <7621705+decanus@users.noreply.github.com>
2021-03-12 18:06:05 +0000

    fix

--- Sources/UI/Room/RoomMemberCell.swift
+++ Sources/UI/Room/RoomMemberCell.swift
@@ -109,7 +109,7 @@ class RoomMemberCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func setup(name: String, image: String, muted: Bool, role: RoomState.RoomMember.Role) {
+    func setup(name: String, image: String, muted: Bool, role: Soapbox_V1_RoomState.RoomMember.Role) {
         user = 0
         muteView.isHidden = true
         speakingView.isHidden = true
@@ -137,7 +137,7 @@ class RoomMemberCell: UICollectionViewCell {
         isMuted = muted
     }
 
-    func setup(member: RoomState.RoomMember) {
+    func setup(member: Soapbox_V1_RoomState.RoomMember) {
         setup(
             name: member.displayName,
             image: member.image,
