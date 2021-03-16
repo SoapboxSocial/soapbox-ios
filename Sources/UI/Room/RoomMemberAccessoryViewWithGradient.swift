80be2c4b3738c81a54e7ef5e769ae4d7d7e57b00
decanus <7621705+decanus@users.noreply.github.com>
2021-03-16 16:20:28 +0000

    idk

--- Sources/UI/Room/RoomMemberAccessoryViewWithGradient.swift
+++ Sources/UI/Room/RoomMemberAccessoryViewWithGradient.swift
@@ -12,6 +12,7 @@ class RoomMemberAccessoryViewWithGradient: RoomMemberAccessoryView {
         layer.locations = [0, 1]
         layer.startPoint = CGPoint(x: 0.25, y: 0.5)
         layer.endPoint = CGPoint(x: 0.75, y: 0.5)
+        layer.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
         layer.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 1, b: 1, c: -1, d: 1, tx: 0.5, ty: -0.5))
         return layer
     }()
@@ -21,12 +22,6 @@ class RoomMemberAccessoryViewWithGradient: RoomMemberAccessoryView {
         layer.insertSublayer(gradient, at: 0)
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        layer.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
-    }
-
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
