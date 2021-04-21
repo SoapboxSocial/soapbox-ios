11889161ec274711fb7dbe9af1681fbe8e23c5d8
decanus <7621705+decanus@users.noreply.github.com>
2021-04-21 22:50:23 +0200

    started playing around.

--- Sources/Scenes/NotificationPrompt/NotificationPromptViewController.swift
+++ Sources/Scenes/NotificationPrompt/NotificationPromptViewController.swift
@@ -18,21 +18,34 @@ class NotificationPromptViewController: DrawerViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(button)
 
-        let text = UILabel()
-        text.text = "oh no!"
-        text.textAlignment = .center
-        text.translatesAutoresizingMaskIntoConstraints = false
-        text.font = .rounded(forTextStyle: .title1, weight: .bold)
-        view.addSubview(text)
+        let title = UILabel()
+        title.text = "oh no!"
+        title.textAlignment = .center
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.font = .rounded(forTextStyle: .title1, weight: .bold)
+        view.addSubview(title)
+
+        let description = UILabel()
+        description.text = "It looks like you have your notifications turned off, to get notified about more rooms like this one why not turn them back on?\n\n Our settings allow you to customize them to your preference!"
+        description.numberOfLines = 0
+        description.translatesAutoresizingMaskIntoConstraints = false
+        description.font = .rounded(forTextStyle: .body, weight: .bold)
+        view.addSubview(description)
 
         NSLayoutConstraint.activate([
-            text.topAnchor.constraint(equalTo: handle.bottomAnchor, constant: 20),
-            text.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
-            text.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            title.topAnchor.constraint(equalTo: handle.bottomAnchor, constant: 20),
+            title.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            title.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
         ])
 
         NSLayoutConstraint.activate([
-            button.topAnchor.constraint(equalTo: text.bottomAnchor, constant: 20),
+            description.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 10),
+            description.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            description.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+        ])
+
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: description.bottomAnchor, constant: 20),
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
             button.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
