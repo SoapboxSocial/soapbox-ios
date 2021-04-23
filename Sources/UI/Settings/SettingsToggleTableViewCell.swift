76a597ebaadcb60dfe71cac0404eea8163a7d8b5
decanus <7621705+decanus@users.noreply.github.com>
2021-04-23 15:17:25 +0200

    reuse

--- Sources/UI/Settings/SettingsToggleTableViewCell.swift
+++ Sources/UI/Settings/SettingsToggleTableViewCell.swift
@@ -26,6 +26,11 @@ class SettingsToggleTableViewCell: UITableViewCell {
         toggle.addTarget(self, action: #selector(toggled), for: .valueChanged)
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        toggle.removeTarget(self, action: #selector(toggled), for: .valueChanged)
+    }
+
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
