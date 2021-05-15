1c2bd0817fa5ac06a71943ddcb678addf1bef69b
decanus <7621705+decanus@users.noreply.github.com>
2021-05-16 01:02:37 +0200

    started on modal

---
+++ Sources/Scenes/FollowRecommendations/FollowRecommendationsViewController.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+class FollowRecommendationsViewController: DrawerViewController {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .rounded(forTextStyle: .largeTitle, weight: .heavy)
+        label.textColor = .white
+        label.text = NSLocalizedString("FollowRecommendations.Title", comment: "")
+        return label
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .rounded(forTextStyle: .body, weight: .semibold)
+        label.text = NSLocalizedString("FollowRecommendations.Description", comment: "")
+        label.textColor = .white
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        manager.drawer.backgroundColor = .brandColor
+
+        NSLayoutConstraint.activate([
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            titleLabel.topAnchor.constraint(equalTo: handle.topAnchor, constant: 30),
+        ])
+
+        NSLayoutConstraint.activate([
+            descriptionLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            descriptionLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 10),
+        ])
+
+        let seperator = UIView()
+        seperator.translatesAutoresizingMaskIntoConstraints = false
+        seperator.backgroundColor = .lightBrandColor
+        seperator.layer.cornerRadius = 1
+        view.addSubview(seperator)
+
+        NSLayoutConstraint.activate([
+            seperator.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            seperator.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            seperator.topAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 20),
+            seperator.heightAnchor.constraint(equalToConstant: 2),
+        ])
+
+        let youMayKnow = UILabel()
+        youMayKnow.translatesAutoresizingMaskIntoConstraints = false
+        youMayKnow.textColor = .white
+        youMayKnow.text = NSLocalizedString("FollowRecommendations.YouMayKnow", comment: "").uppercased()
+        youMayKnow.font = .boldSystemFont(ofSize: 17)
+        view.addSubview(youMayKnow)
+    }
+}
