bcea1c8c50c80a7cbeaa9934ba71b99019fc21ad
Jeff Reiner <theundeadghost@gmail.com>
2021-05-28 14:21:59 +0200

    Making things into components

--- Sources/Scenes/Profile/ProfileView.swift
+++ Sources/Scenes/Profile/ProfileView.swift
@@ -7,7 +7,55 @@
 
 import SwiftUI
 
+enum IconButtonStyle {
+    case primary, secondary
+}
+
+struct IconButton: View {
+    var icon: String;
+    var type: IconButtonStyle = .primary
+    var action: () -> Void;
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 16, weight: .semibold))
+            .frame(width: 40, height: 40)
+            .foregroundColor(type == .primary ? .white : .primary)
+            .background(type == .primary ? Color(.systemPurple) : Color(.systemGray5))
+            .clipShape(Circle())
+    }
+}
+
+struct PillButton: View {
+    var text: String;
+    var action: () -> Void;
+    
+    var body: some View {
+        Text(text)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .frame(height: 40)
+            .padding(.horizontal, 20)
+            .background(Color(.systemPurple))
+            .clipShape(Capsule())
+    }
+}
+
+struct ProfilePicture: View {
+    var body: some View {
+        Image(systemName: "")
+            .frame(width: 80, height: 80)
+            .background(Color(.systemPurple))
+            .clipShape(Circle())
+    }
+}
+
 struct ProfileView: View {
+    var display_name: String = "Amy"
+    var bio: String = "About Me. Think you can beat my Flappy Bird score? Try me."
+    var followers_count: String = "1,100"
+    var following_count: String = "2,400"
+    
     @State private var isFollowing = true
     @State private var isNotifying = true
     
@@ -22,70 +70,35 @@ struct ProfileView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             VStack(alignment: .leading, spacing: 10) {
-                Image(systemName: "")
-                    .frame(width: 80, height: 80)
-                    .background(Color(.systemPurple))
-                    .clipShape(Circle())
+                ProfilePicture()
                 
-                Text("Amy").font(.system(size: 24)).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Text(display_name).font(.system(size: 24)).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 
-                Text("About Me. Think you can beat my Flappy Bird score? Try me.")
+                Text(bio)
             }
             
             HStack(spacing: 10) {
-                Text("2,400").bold() + Text(" ") + Text("Following").foregroundColor(.secondary)
+                Text(following).bold() + Text(" ") + Text("following_count").foregroundColor(.secondary)
                 
-                Text("1,100").bold() + Text(" ") + Text("Followers").foregroundColor(.secondary)
+                Text(followers_count).bold() + Text(" ") + Text("Followers").foregroundColor(.secondary)
             }
             
             HStack(spacing: 10) {
                 if isFollowing {
-                    Text("Following")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(height: 40)
-                        .padding(.horizontal, 20)
-                        .background(Color(.systemPurple))
-                        .clipShape(Capsule())
+                    PillButton(text: "Following", action: toggleFollowing)
                 } else {
-                    Text("Follow")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(height: 40)
-                        .padding(.horizontal, 20)
-                        .background(Color(.systemPurple))
-                        .clipShape(Capsule())
+                    PillButton(text: "Follow", action: toggleFollowing)
                 }
                 
                 if isNotifying {
-                    Image(systemName: "bell.fill")
-                        .font(.system(size: 16, weight: .semibold))
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(.white)
-                        .background(Color.purple)
-                        .clipShape(Circle())
+                    IconButton(icon: "bell.fill", action: toggleNotifying)
                 } else {
-                    Image(systemName: "bell")
-                        .font(.system(size: 16, weight: .semibold))
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(.primary)
-                        .background(Color(.systemGray5))
-                        .clipShape(Circle())
+                    IconButton(icon: "bell", type: .secondary, action: toggleNotifying)
                 }
                 
-                Image(systemName: "link")
-                    .font(.system(size: 16, weight: .semibold))
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(.primary)
-                    .background(Color(.systemGray5))
-                    .clipShape(Circle())
+                IconButton(icon: "link", type: .secondary, action: {})
                 
-                Image(systemName: "link")
-                    .font(.system(size: 16, weight: .semibold))
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(.primary)
-                    .background(Color(.systemGray5))
-                    .clipShape(Circle())
+                IconButton(icon: "link", type: .secondary, action: {})
             }
             
             Spacer()
