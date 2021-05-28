ae06a553befabecda3ed7c5a51fc6fe6997cbb8f
Jeff Reiner <theundeadghost@gmail.com>
2021-05-28 18:09:38 +0200

    Fix

--- Sources/UI/Room/ButtonBar.swift
+++ Sources/UI/Room/ButtonBar.swift
@@ -23,7 +23,7 @@ class ButtonBar<E: Item>: UIView where E.RawValue == String {
 
     private let iconConfig = UIImage.SymbolConfiguration(weight: .semibold)
 
-    private(set) var buttons = [E: SoapButton]()
+    private(set) var buttons = [E: Button]()
 
     weak var delegate: ButtonBarDelegate?
 
@@ -37,7 +37,7 @@ class ButtonBar<E: Item>: UIView where E.RawValue == String {
         var anchor: NSLayoutXAxisAnchor!
 
         for item in E.allCases {
-            let button = SoapButton()
+            let button = Button()
             button.translatesAutoresizingMaskIntoConstraints = false
             button.setImage(UIImage(systemName: item.icon(), withConfiguration: iconConfig), for: .normal)
             button.tintColor = .brandColor
